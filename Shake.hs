4bef158c770d5d4eb20f79123ecfdcb3fff14f71
Brent Yorgey <byorgey@gmail.com>
2015-01-28 15:08:58 -0500

    make framed environment for commentary, a Shake file, and a bit more translation

---
+++ Shake.hs
@@ -0,0 +1,23 @@
+import           Development.Shake
+import           Development.Shake.FilePath
+
+lhs2TeX, pdflatex, rubber :: String
+lhs2TeX  = "lhs2TeX"
+pdflatex = "pdflatex"
+rubber = "rubber"
+
+main :: IO ()
+main = shake shakeOptions $ do
+
+    want ["series-formelles.pdf"]
+
+    -- "*.tex" %> \output -> do
+    --     let input = replaceExtension output "lhs"
+    --     need [input]
+    --     cmd lhs2TeX $ ["-o", output] ++ [input]
+
+    "*.pdf" %> \output -> do
+        let input = replaceExtension output "tex"
+        need [input]
+        () <- cmd pdflatex $ ["--enable-write18", input]
+        cmd rubber $ ["-f", "-d", input]
