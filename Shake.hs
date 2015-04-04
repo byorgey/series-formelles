ef8c6d5bf50df59ec909086ea1dadc8d041ea097
Brent Yorgey <byorgey@gmail.com>
2015-04-03 21:51:49 -0400

    some editing and commentary

--- Shake.hs
+++ Shake.hs
@@ -1,5 +1,6 @@
 import           Development.Shake
 import           Development.Shake.FilePath
+import           System.Exit
 
 lhs2TeX, pdflatex, rubber :: String
 lhs2TeX  = "lhs2TeX"
@@ -9,15 +10,19 @@ rubber = "rubber"
 main :: IO ()
 main = shake shakeOptions $ do
 
-    want ["series-formelles.pdf"]
+  want ["series-formelles.pdf"]
 
-    -- "*.tex" %> \output -> do
-    --     let input = replaceExtension output "lhs"
-    --     need [input]
-    --     cmd lhs2TeX $ ["-o", output] ++ [input]
+  -- "*.tex" %> \output -> do
+  --     let input = replaceExtension output "lhs"
+  --     need [input]
+  --     cmd lhs2TeX $ ["-o", output] ++ [input]
 
-    "*.pdf" %> \output -> do
-        let input = replaceExtension output "tex"
-        need [input]
-        () <- cmd pdflatex $ ["--enable-write18", input]
-        cmd rubber $ ["-f", "-d", input]
+  "*.pdf" %> \output -> do
+      let input = replaceExtension output "tex"
+      need [input]
+      cmd pdflatex $ ["--enable-write18", input]
+      -- Exit c <- cmd rubber $ ["-f", "-d", input]
+      -- case c of
+      --   ExitFailure _ -> do
+      --     cmd "mklatex" $ ["-pdf", input]
+      --   _ -> return ()
