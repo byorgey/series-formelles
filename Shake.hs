a7c144713980a0908df1926d8eb6c691e9e62037
Brent Yorgey <byorgey@gmail.com>
2018-02-08 00:57:10 -0600

    try to work on sandboxed Shake

--- Shake.hs
+++ Shake.hs
@@ -1,3 +1,23 @@
+#!/usr/bin/env stack
+{- stack
+   --resolver lts-9.21
+   --install-ghc
+   runghc
+   --package diagrams-pgf-1.4
+   --package multiset-comb-0.2.4.1
+   --package texrunner-0.0.1.1
+   --package lens-4.16
+   --package palette-0.1.0.5
+   --package split-0.2.3.3
+   --package containers-0.5.11.0
+   --package diagrams-core-1.4.0.1
+   --package diagrams-lib-1.4.2
+   --package diagrams-contrib-1.4.1
+   --package SVGFonts-1.6.0.3
+   --package shake-0.16
+   --package diagrams-builder-0.8.0.2
+-}
+
 import           Development.Shake
 import           Development.Shake.FilePath
 import           System.Exit
@@ -12,17 +32,7 @@ main = shake shakeOptions $ do
 
   want ["series-formelles.pdf"]
 
-  -- "*.tex" %> \output -> do
-  --     let input = replaceExtension output "lhs"
-  --     need [input]
-  --     cmd lhs2TeX $ ["-o", output] ++ [input]
-
   "*.pdf" %> \output -> do
       let input = replaceExtension output "tex"
       need [input]
       cmd pdflatex $ ["--enable-write18", input]
-      -- Exit c <- cmd rubber $ ["-f", "-d", input]
-      -- case c of
-      --   ExitFailure _ -> do
-      --     cmd "mklatex" $ ["-pdf", input]
-      --   _ -> return ()
